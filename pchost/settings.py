c8463e09336cc615c11069d465794933e41e1b89
Shirambere Edward <sedward@Shiramberes-MacBook-Pro.local>
2023-11-11 00:02:40 +0300

    added assets and static files settings

--- pchost/settings.py
+++ pchost/settings.py
@@ -112,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# other static file locations
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- pchost/urls.py
+++ pchost/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from check_onboarded import is_onboarded
+from .check_onboarded import is_onboarded
 
 user_onboarded = is_onboarded()
 
