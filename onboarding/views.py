5b043be188de4115e8b3dde2919a03ab7d9139bf
Shirambere Edward <sedward@Shiramberes-MacBook-Pro.local>
2023-11-13 22:39:15 +0300

    check post data

--- onboarding/views.py
+++ onboarding/views.py
@@ -5,4 +5,6 @@ def login(request):
     return render(request, 'pages/signin.html')
 
 def register(request):
+    if request.method == "POST":
+        print(request.POST)
     return render(request, 'pages/signup.html')
